a1c4d164949c7a5c80f8053cb41d9a017b2a58fa
shayc331 <shayc331@cs.huji.ac.il>
2022-07-10 15:43:28 +0300

    - calculate p directly using svd - function for camera calibration

--- main.py
+++ main.py
@@ -12,6 +12,31 @@ FLANN_INDEX_KDTREE = 0
 IM1_PATH = os.path.join(os.getcwd(), r'input\images\same_side\1_05.jpg')
 IM2_PATH = os.path.join(os.getcwd(), r'input\images\same_side\2_05.jpg')
 
+# points we choose in the images with corresponding real points
+# same view im1:
+points1 = np.array([[753, 664, 1],
+                    [65, 415, 1],
+                    [1097, 484, 1],
+                    [1127, 378, 1],
+                    [1194, 281, 1],
+                    [930, 339, 1]])
+
+real_points = np.array([[0, 0, 0, 1],
+                        [0, 9, 0, 1],
+                        [8, 0, 0, 1],
+                        [8, 0, 1.42, 1],
+                        [8, -0.91, 2.44, 1],
+                        [9, 2.2, 1.7, 1]])  # the corresponding real points
+
+# same view im2:
+points2 = np.array([[575, 513, 1],
+                    [172, 410, 1],
+                    [103, 390, 1],
+                    [1053, 291, 1],
+                    [1107, 199, 1],
+                    [884, 274, 1]])
+
+
 def findMatchingPoints(kp1, des1, kp2, des2, d):
     index_params = dict(algorithm=FLANN_INDEX_KDTREE, trees=5)
     search_params = dict(checks=50)  # or pass empty dictionary
@@ -138,13 +163,36 @@ def get_constraints(p1, p2):
 
 
 def findHomography(points, real_points):
-    H = np.ones((15, 16))
+    H = np.ones((18, 16))
     for i, (p, real_p) in enumerate(zip(points, real_points)):
         a = get_constraints(p, real_p)
         H[i * 3: i * 3 + 3] = a
     _, _, vt = np.linalg.svd(H)
     return vt[-1].reshape(4, 4)
 
+
+def get_eq(p_img, p_real):
+    X, Y, Z, _ = p_real
+    u, v, _ = p_img
+    return np.array([
+        [X, Y, Z, 1, 0, 0, 0, 0, -u*X, -u*Y, -u*Z, -u],
+        [0, 0, 0, 0, X, Y, Z, 1, -v*X, -v*Y, -v*Z, -v]
+    ])
+
+
+def find_projection_matrix(points, real_points):
+    H = np.ones((12, 12))
+    for i, (p, real_p) in enumerate(zip(points, real_points)):
+        a = get_eq(p, real_p)
+        H[i * 2: i * 2 + 2] = a
+    _, _, vt = np.linalg.svd(H)
+    return vt[-1].reshape(3, 4)
+
+
+def calculate_P1_P2():
+    P1 = find_projection_matrix(points1, real_points)
+    P2 = find_projection_matrix(points2, real_points)
+    return P1, P2
 
 def get_world_points(P, points):
     world_points = []
@@ -156,38 +204,42 @@ def get_world_points(P, points):
         world_points.append(X)
     return world_points
 
-def clibrate_cameras(P1,P2):
+
+def calibrate_cameras(P1,P2):
     # points we choose in the images with corresponding real points
     #same view im1:
     points1 = np.array([[753,664, 1],
                         [65, 415, 1],
                         [1097, 484, 1],
                         [1127, 378, 1],
-                        [1194, 281, 1]])
+                        [1194, 281, 1],
+                        [930, 339, 1]])
 
     world_points1 = get_world_points(P1, points1)
-    real_points1 = np.array([[0, 0, 0, 1],
-                             [0,9,0,1],
-                             [8,0,0,1],
-                             [8,0,1.42,1],
-                             [8,-0.91, 2.44, 1]])  # the corresponding real points
+    real_points = np.array([[0, 0, 0, 1],
+                             [0, 9, 0, 1],
+                             [8, 0, 0, 1],
+                             [8, 0, 1.42, 1],
+                             [8, -0.91, 2.44, 1],
+                             [9, 2.2, 1.7, 1]])  # the corresponding real points
 
     #same view im2:
     points2 = np.array([[575, 513, 1],
                         [172, 410, 1],
                         [103, 390, 1],
                         [1053, 291, 1],
-                        [1107, 199, 1]])  # points we choose in the images with corresponding real points
+                        [1107, 199, 1],
+                        [884, 274, 1]])  # points we choose in the images with corresponding real points
     world_points2 = get_world_points(P2, points2)
-    real_points2 = np.array([[0,0,0,1]])  # the corresponding real points
 
+
+    # P1 = find_projection_matrix(points1, real_points)
     #todo: see if this method will work for 4*4 points....
-    H1 = findHomography(world_points1, real_points1)  # works with MSE to find optimal H
-    H2 = findHomography(world_points2, real_points2)
+    H1 = findHomography(world_points1, real_points)  # works with MSE to find optimal H
+    H2 = findHomography(world_points2, real_points)
 
     return P1 @ np.linalg.inv(H1), P2 @ np.linalg.inv(H2)
     # return P1 @ H1, P2 @ H2
-
 
 
 def compute_3d(x1, x2, P1, P2):
@@ -208,19 +260,64 @@ def compute_3d(x1, x2, P1, P2):
     return vt[-1]
 
 
+
+def calibrate_camera_matrix():
+    img = cv2.imread(f"chess.jpg", 1)
+
+    objp = np.zeros((6 * 7, 3), np.float32)
+    objp[:, :2] = (np.mgrid[0:7, 0:6].T.reshape(-1, 2)) * 30  # the board squares are 30 cm
+    print(objp)
+    # Color-segmentation to get binary mask
+    lwr = np.array([0, 0, 180])  # need to cut the image to include only the board
+    upr = np.array([179, 61, 252])
+    hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+    msk = cv2.inRange(hsv, lwr, upr)
+
+    # Extract chess-board
+    krn = cv2.getStructuringElement(cv2.MORPH_RECT, (50, 30))
+    dlt = cv2.dilate(msk, krn, iterations=5)
+    res = 255 - cv2.bitwise_and(dlt, msk)
+
+    # Displaying chess-board features
+    res = np.uint8(res)
+    # call imshow() using plt object
+    plt.imshow(res, cmap='gray')
+
+    # display that image
+    plt.show()
+    ret, corners = cv2.findChessboardCorners(res, (7, 6),
+                                             flags=cv2.CALIB_CB_ADAPTIVE_THRESH +
+                                                   cv2.CALIB_CB_FAST_CHECK +
+                                                   cv2.CALIB_CB_NORMALIZE_IMAGE)
+    if ret:
+        print(corners)
+        # fnl = cv2.drawChessboardCorners(img, (7, 6), corners, ret)
+        # cv2.imshow("fnl", fnl)
+        # cv2.waitKey(0)
+    else:
+        print("No Checkerboard Found")
+
+    ret, mtx, dist, rvecs, tvecs = cv2.calibrateCamera([objp], [corners], res.shape[::-1], None, None)
+    print(f"mtx: {mtx}")
+    print(f"dist: {dist}")
+    print(f"rvecs: {rvecs}")
+    print(f"tvecs: {tvecs}")
+
 if __name__ == '__main__':
     img1 = cv2.imread(IM1_PATH, 0)
     img2 = cv2.imread(IM2_PATH, 0)
 
-    F = get_fundamental(img1, img2)
-    P1, P2 = get_cameras_matrix(F)
-    P1M, P2M = clibrate_cameras(P1,P2)
+    # F = get_fundamental(img1, img2)
+    P1, P2 = calculate_P1_P2()
+    # P1, P2 = get_cameras_matrix(F)
+    # P1M, P2M = calibrate_cameras(P1, P2)
 
     # todo - manualy set image coordinates of the ball.
     point1 = np.array([592, 22, 1])
     point2 = np.array([445, 77, 1])
 
-    X = compute_3d(point1, point2, P1M, P2M)
+    # X = compute_3d(point1, point2, P1M, P2M)
+    X = compute_3d(point1, point2, P1, P2)
     print(X[:3] / X[3])
     # the height will be the z coordinate of X.
 
